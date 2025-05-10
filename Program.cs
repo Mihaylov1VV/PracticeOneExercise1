5b603074d80f930d7addabf9f90531d176c3c78c
Mihaylov1VV <vitalii.mihailov1995@gmail.com>
2025-05-10 12:34:19 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercise1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Задание номер 1:
+        //Объявите переменную следующего вида
+        uint you_age;
+        Console.WriteLine(you_age = 10);
+        string name;
+        Console.WriteLine(name = "Vitaliy");
+        string company_name;
+        Console.WriteLine(company_name = "Directum");
+        bool man;
+        Console.WriteLine(man = true);
+        float weight;
+        Console.WriteLine(weight = 63.5f );
+    }
+}
